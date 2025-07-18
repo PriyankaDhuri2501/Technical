50dc970b0b5289405b404e45e16e4b3867416fa4
Priyanka Dhuri <149864623+PriyankaDhuri2501@users.noreply.github.com>
2025-07-18 22:18:21 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,21 @@
+# i=0
+# while i< len(clr):
+#     print(clr[i].title())
+#     i+=1
+
+# num=1
+# while(num<=100):
+#     print(num)
+#     num+=1
+
+# x=1
+# while(x<=100):
+#     a = int(input("Enter value:"))
+#     if(a<100 and a>1):
+#         print("Belongs in 1-100")
+#         break
+#     else:
+#         print("Doesn't belong to 1-100")
+
+#print number from 1-100 and 100-1
+#find the sum of first n numbers using whilessss
